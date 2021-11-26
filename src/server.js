5815dc5663dc06b743bfa59dde3ec49b7f164f06
Cristian Stenico <cristianstenico@gmail.com>
2021-11-26 23:43:33 +0100

    Add server origin (for CORS)

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ import game from './game.js'
 const server = Server({
   games: [game],
   origins: [
-    Origins.LOCALHOST_IN_DEVELOPMENT
+    Origins.LOCALHOST_IN_DEVELOPMENT,
+    'https://proloco.netlify.app'
   ]
 })
 const PORT = process.env.PORT || 8000
